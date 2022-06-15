e074cd604abc24e5d7f3fd6c5760ed9badd58aca
Martin Vanaud <martin.vanaud@epitech.eu>
2022-06-15 16:10:55 +0200

    added AudioResultComponent for victor

---
+++ Source/Audio/AudioResultComponent.cpp
@@ -0,0 +1,113 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.7
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+//[Headers] You can add your own extra header files here...
+#include <memory>
+#include <iostream>
+//[/Headers]
+
+#include "AudioResultComponent.h"
+
+//[MiscUserDefs] You can add your own user definitions and misc code here...
+//[/MiscUserDefs]
+
+//==============================================================================
+AudioResultComponent::AudioResultComponent() :
+        state (Stopped),
+        thumbnailCache (5),
+        thumbnailComp (512, formatManager, thumbnailCache),
+        positionOverlay (transportSource),
+        thumbnailResultComp(512, formatManager, thumbnailCache)
+{
+    //[Constructor_pre] You can add your own custom stuff here..
+    //[/Constructor_pre]
+
+    startTimer(100);
+
+    addAndMakeVisible (&thumbnailComp);
+    addAndMakeVisible (&positionOverlay);
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    addAndMakeVisible(&playButton);
+    playButton.setButtonText(TRANS("Play"));
+    playButton.onClick = [this]
+    { playButtonClicked(); };
+    playButton.setEnabled(false);
+    playButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
+
+    playButton.setBounds(200, 650, 50, 30);
+
+    addAndMakeVisible(&stopButton);
+    stopButton.setButtonText(TRANS("Stop"));
+    stopButton.onClick = [this]
+    { stopButtonClicked(); };
+    stopButton.setEnabled(false);
+    stopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
+
+    stopButton.setBounds(270, 650, 50, 30);
+
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener (this);
+
+    setAudioChannels (2, 2);
+
+    //[UserPreSize]
+    //[/UserPreSize]
+
+    //[Constructor] You can add your own custom stuff here..
+    //[/Constructor]
+}
+
+AudioResultComponent::~AudioResultComponent()
+{
+    //[Destructor_pre]. You can add your own custom destruction code here..
+    //[/Destructor_pre]
+
+    shutdownAudio();
+
+    //[Destructor]. You can add your own custom destruction code here..
+    //[/Destructor]
+}
+
+
+//==============================================================================
+void AudioResultComponent::paint(Graphics &g)
+{
+    //[UserPrePaint] Add your own custom painting code here..
+    //[/UserPrePaint]
+
+    // g.fillAll(juce::Colours::black); // A VOIR
+
+    //[UserPaint] Add your own custom painting code here..
+    //[/UserPaint]
+}
+
+void AudioResultComponent::resized()
+{
+    //[UserPreResize] Add your own custom resize code here..
+    //[/UserPreResize]
+
+    juce::Rectangle<int> thumbnailBounds (10, 670, getWidth() - 20, 100);
+    thumbnailComp.setBounds (thumbnailBounds);
+    positionOverlay.setBounds (thumbnailBounds);
+
+    //[UserResized] Add your own custom resize handling here..
+    //[/UserResized]
+}
---
+++ Source/Audio/AudioResultComponent.h
@@ -0,0 +1,200 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.4.7
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include <unistd.h>
+
+#include <JuceHeader.h>
+
+#include "ProcessorComponent.h"
+
+#include "Thumbnails/Tools/SimplePeakModelization.h"
+#include "Thumbnails/Tools/SimplePositionOverlay.h"
+#include "Thumbnails/Tools/SimpleThumbnailComponent.h"
+
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class AudioResultComponent  : public juce::AudioAppComponent,
+                      public juce::ChangeListener
+{
+public:
+    //==============================================================================
+    AudioResultComponent ();
+    ~AudioResultComponent() override;
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void timerCallback() override;
+
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
+    {
+        transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+    }
+
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override
+    {
+        if (readerSource.get() == nullptr)
+            bufferToFill.clearActiveBufferRegion();
+    }
+
+    void releaseResources() override
+    {
+        transportSource.releaseResources();
+    }
+
+    void changeListenerCallback (juce::ChangeBroadcaster* source) override
+    {
+        if (source == &transportSource)
+            transportSourceChanged();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    void paint (Graphics& g) override;
+    void resized() override;
+    void sliderValueChanged (Slider* sliderThatWasMoved) override;
+    void buttonClicked (Button* buttonThatWasClicked) override;
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    // GET Informations from processor
+
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+
+    void changeState (TransportState newState)
+    {
+        if (state != newState)
+        {
+            state = newState;
+
+            switch (state)
+            {
+                case Stopped:
+                    stopButton.setEnabled (false);
+                    playButton.setEnabled (true);
+                    transportSource.setPosition (0.0);
+                    break;
+
+                case Starting:
+                    playButton.setEnabled (false);
+                    transportSource.start();
+                    break;
+
+                case Playing:
+                    stopButton.setEnabled (true);
+                    break;
+
+                case Stopping:
+                    transportSource.stop();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    void transportSourceChanged()
+    {
+        if (transportSource.isPlaying())
+            changeState (Playing);
+        else
+            changeState (Stopped);
+    }
+
+    void openAudioTrack()
+    {
+        juce::File resultFile(processor->getPath() + "/examples/CMake/BeatBox/Musics/.temp/JUCE_Demo_Audio_Recording2-transferred.wav");
+
+        auto *reader = formatManager.createReaderFor (resultFile);
+        // juce::AudioSampleBuffer buffer(1, reader->lengthInSamples);
+
+        // std::string filename = file.getFileNameWithoutExtension().toStdString();
+
+        if (reader != nullptr)
+        {
+            auto newSource = std::make_unique<juce::AudioFormatReaderSource> (reader, true);
+            resultTransportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);
+            playBtn.setEnabled (true);
+            readerSource.reset (newSource.release());
+        }
+    }
+
+    void playButtonClicked()
+    {
+        changeResultState(Starting);
+    }
+
+    void stopButtonClicked()
+    {
+        changeResultState(Stopping);
+    }
+
+    std::string pathToFile;
+
+    juce::TextButton playButton;
+    juce::TextButton stopButton;
+
+
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    TransportState state;
+
+    juce::AudioThumbnailCache thumbnailCache;
+    SimpleThumbnailComponent thumbnailComp;
+    SimplePositionOverlay positionOverlay;
+
+    juce::File file;
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    std::unique_ptr<ProcessorComponent> processor = std::make_unique<ProcessorComponent>();
+
+    //[/UserVariables]
+
+    //==============================================================================
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioResultComponent)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
